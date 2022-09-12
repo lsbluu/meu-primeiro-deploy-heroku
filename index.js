a51701a2e41c559054f39fe2a99b40e5d6209ac0
lsblu <lsblu@hotmail.com>
2022-09-12 11:35:45 -0300

    teste

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => res.send(`<h1>Executando na porta ${PORT}</h1>`))
+app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
+
