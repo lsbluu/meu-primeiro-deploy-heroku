238f41f062f34f04faeb07b565de29ac184079e2
lsblu <lsblu@hotmail.com>
2022-09-13 10:47:36 -0300

    teste

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send(`<h1>EExecutando na porta ${PORT}</h1>`));
+app.get('/', (req, res) => res.send(`<h1>EEExecutando na porta ${PORT}</h1>`));
 app.listen(PORT, () => console.log(`RRRodando na porta ${PORT}`));
